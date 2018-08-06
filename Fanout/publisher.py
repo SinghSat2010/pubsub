03eefb52bbb1eccb6035f5dd23677307a7a69737
SinghSat2010 <singhsat2010@gmail.com>
2018-08-06 12:06:52 -0400

    Update publisher.py

--- Fanout/publisher.py
+++ Fanout/publisher.py
@@ -9,6 +9,6 @@ channel.exchange_declare(exchange='person', exchange_type='fanout')
 message = ''.join(sys.argv[1:]) or "ss4878"
 channel.basic_publish(exchange='person', routing_key='', body=message)
 
-print(" [x] sent %r" % message)
+print(" [x] entity sent %r" % message)
 
 connection.close()
